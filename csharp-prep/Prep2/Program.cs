9e0ce695a6d26aacc2fa6acc458edadf08953c63
Austin Murdock <aumu870@gmail.com>
2024-04-24 18:34:21 -0600

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letterGrade = "Z";
+
+        Console.WriteLine();
+
+        Console.Write("Enter your grade, as an integer: ");
+        int value = int.Parse(Console.ReadLine());
+
+        if (value >= 90) {
+            letterGrade = "A";
+        }
+        else if (value >= 80) {
+            letterGrade = "B";
+        }
+        else if (value >= 70) {
+            letterGrade = "C";
+        }
+        else if (value >= 60) {
+            letterGrade = "D";
+        }
+        else {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is \"{letterGrade}\".");
+
+        Console.WriteLine("\n");
     }
 }
