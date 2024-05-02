37a9a73ebbeefbfc00e5d0cdbc0189054235936e
Austin Murdock <aumu870@gmail.com>
2024-05-02 00:57:22 -0600

    finished Prep5 assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,46 @@
 using System;
+// using Internal;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+class Program {
+    static void Main(string[] args) {
+        string usersName;
+        int usersInt;
+        int usersResult;
+
+        Console.WriteLine();
+
+        DisplayWelcome();
+        usersName = PromptUserName();
+        usersInt = PromptUserInt();
+        usersResult = SquareNumber(usersInt);
+        DisplayResult(usersName, usersResult);
+
+        Console.WriteLine("\n");
+    }
+
+    static void DisplayWelcome() {
+        Console.WriteLine(" - Program Start - ");
+    }
+
+    static string PromptUserName() {
+        Console.Write("Enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserInt() {
+        Console.Write("Enter an integer: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int input) {
+        // Math.Pow returns a "double" data type, 
+        // can be converted by (int) casting or Math.Truncate, which both floor,
+        // or Convert.ToInt32, which rounds - storage not being variable is annoying though:
+        return Convert.ToInt32(Math.Pow(input, 2));
+        // can also just (input * input) in this situation
+    }
+
+    static void DisplayResult(string name, int square) {
+        Console.WriteLine($"\"{name}\"; User-input integer squared: {square}");
     }
 }
