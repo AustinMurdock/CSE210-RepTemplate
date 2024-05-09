07e53faebe7284f0a0ce75b3ccb1a9865ad16315
Austin Murdock <aumu870@gmail.com>
2024-05-09 01:47:58 -0600

    w03 Learning02 assignment, added Job.cs class and Main() implementation

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Job {
+    // all var types that the Job type contains:
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    // contructor; use to require properties on intance creation
+    public Job() {
+        // this._company = _company;
+        // this._jobTitle = _jobTitle;
+        // // ...
+    }
+
+    public void Display() {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+
+
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,16 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Console.WriteLine("Hello Learning02 World!";
-
-        Person fred = new Person("Fred", "Flinstone"); // seperate instances
-        // fred.givenName = "Fred"; // can't be changed directly when private
-        // fred.familyName = "Flinstone";
-        Person steve = new Person("Steve", "Minecraft");
-        // steve.givenName = "Steve";
-        // steve.familyName = "Minecraft";
+        Console.WriteLine();
         
-        fred.EasternStyleName();
-        steve.WesternStyleName();
+        // Job is now a data type that contains 2 strings and 2 ints:
+        Job job1 = new(); // standard: " = new Job();", simplified
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new() { // more simplified, only for initialization?
+            _jobTitle = "Custodian", // why commas? idk
+            _company = "BYU-I",
+            _startYear = 2023,
+            _endYear = 2024,
+        };
+
+        job1.Display();
+        job2.Display();
+
+        Console.WriteLine("\n");
+        
+
+        // Console.WriteLine("Hello Learning02 World!");
+
+        // Person fred = new Person("Fred", "Flinstone"); // seperate instances
+        // // fred.givenName = "Fred"; // can't be changed directly when private
+        // // fred.familyName = "Flinstone";
+        // Person steve = new Person("Steve", "Minecraft");
+        // // steve.givenName = "Steve";
+        // // steve.familyName = "Minecraft";
+        
+        // fred.EasternStyleName();
+        // steve.WesternStyleName();
     }
 }
