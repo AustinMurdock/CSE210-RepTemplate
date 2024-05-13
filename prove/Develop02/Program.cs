054be02149a6f9df099df9d5d7575515c212f923
Austin Murdock <aumu870@gmail.com>
2024-05-13 16:04:02 -0600

    Develop02 basic Main input loop

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine();
+
+        string userInput;
+        string fileName;
+        
+        while (true) {
+            Console.WriteLine("""
+            Journal Options:
+            1. "write"
+            2. "display"
+            3. "save"
+            4. "load"
+            5. "exit"
+            """);
+            userInput = Console.ReadLine().ToLower();
+
+            if (userInput == "write") {
+                ;
+            }
+            else if (userInput == "display") {
+                ;
+            }
+            else if (userInput == "save") {
+                ;
+            }
+            else if (userInput == "load") {
+                ;
+            }
+            else if (userInput == "exit") {
+                break;
+            }
+            else {
+                Console.WriteLine("Invalid input, try again.");
+            }
+        }
+
+        Console.WriteLine("\n");
+        // Console.WriteLine("Hello Develop02 World!");
     }
 }
